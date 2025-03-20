6ea51fd24137461f05d3b72f7569150370777faa
Rafael Tosta <rafa.ecomp@gmail.com>
2025-03-20 16:35:10 -0300

    Update __main__.py

--- gpaae/__main__.py
+++ gpaae/__main__.py
@@ -1,28 +1,21 @@
-from gpaae.services.CredentialsManager import CredentialsManager
-from gpaae.services.SuapBot import SuapBot
+from gpaae.bots.PaaeBot import PaaeBot
 
 
 def main():
-    print("Gerenciador PAAE")
 
-    manager = CredentialsManager()
-    username, password = manager.get_credentials()
-
-    if username and password:
-
-        print("Realizando login SUAP.")
-        bot = SuapBot(headless=False)  # Executa com interface gráfica
-        bot.perform_login(username, password)
-
-        print("Acessando página de relatórios")
-        bot.get_report_params()
-
-    else:
-        print("Nenhuma credencial disponível.")
-
-    input("Pressione qualquer tecla para finalizar...")
-    bot.close()
+    paae_bot = PaaeBot()
+    paae_bot.exec()
 
 
 if __name__ == "__main__":
     main()
+
+"""
+
+PaaeBot -> Coleta dados para o PAAE
+    Entrada: Planilha com estudantes
+
+ConselhoBot -> Coleta dados para o conselho
+    Entrada: Planilha com código das turmas
+
+"""
